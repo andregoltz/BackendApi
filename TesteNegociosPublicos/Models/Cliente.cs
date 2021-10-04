9fab95194ce8281eaeb936cf3d6d763dc7c37fa9
andregoltz <andregoltz@outlook.com.br>
2021-10-04 17:01:27 -0300

    Setting All the API

--- TesteNegociosPublicos/Models/Cliente.cs
+++ TesteNegociosPublicos/Models/Cliente.cs
@@ -6,11 +6,14 @@ namespace Models
 {
     public class Cliente
     {
+        #region Atributes
         public int Id { get; set; }
         public string Nome { get; set; }
         public string CPF { get; set; }
         public string Email { get; set; }
+        #endregion
 
+        #region Methods
         public static List<Cliente> Todos()
         {
             var lista = new List<Cliente>();
@@ -39,5 +42,57 @@ namespace Models
 
             return lista;
         }
+        public Cliente Salvar()
+        {
+            SqlConnection conn = new SqlConnection(Conexao.Dados);
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO Clientes(nome, CPF, email) values (@nome, @CPF, @email)", conn);
+
+            cmd.Parameters.Add(new SqlParameter("@nome", System.Data.SqlDbType.VarChar, 200) { Value = this.Nome });
+            cmd.Parameters.Add(new SqlParameter("@CPF", System.Data.SqlDbType.VarChar, 11) { Value = this.CPF });
+            cmd.Parameters.Add(new SqlParameter("@email", System.Data.SqlDbType.VarChar, 100) { Value = this.Email });
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            conn.Dispose();
+
+            return this;
+        }
+        public Cliente Atualizar()
+        {
+            SqlConnection conn = new SqlConnection(Conexao.Dados);
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("UPDATE Clientes SET nome=@nome, CPF=@CPF, email=@email WHERE id=@id", conn);
+            cmd.Parameters.Add(new SqlParameter("@id", System.Data.SqlDbType.Int) { Value = this.Id });
+            cmd.Parameters.Add(new SqlParameter("@nome", System.Data.SqlDbType.VarChar, 200) { Value = this.Nome });
+            cmd.Parameters.Add(new SqlParameter("@CPF", System.Data.SqlDbType.VarChar, 11) { Value = this.CPF });
+            cmd.Parameters.Add(new SqlParameter("@email", System.Data.SqlDbType.VarChar, 100) { Value = this.Email });
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            conn.Dispose();
+
+            return this;
+        }
+        public static bool Excluir(int id)
+        {
+            SqlConnection conn = new SqlConnection(Conexao.Dados);
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("DELETE FROM Clientes WHERE id=@id", conn);
+            cmd.Parameters.Add(new SqlParameter("@id", System.Data.SqlDbType.Int) { Value = id});
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            conn.Dispose();
+
+            return true;
+        }
+        #endregion
     }
 }
--- TesteNegociosPublicos/TesteNegociosPublicos/Controllers/ClientesController.cs
+++ TesteNegociosPublicos/TesteNegociosPublicos/Controllers/ClientesController.cs
@@ -9,9 +9,32 @@ namespace TesteNegociosPublicos.Controllers
     public class ClientesController : ControllerBase
     {
         [HttpGet]
+        [Route("lista")]
         public List<Cliente> Index()
         {
             return Cliente.Todos();
+        }
+
+        [HttpPost]
+        [Route("criar")]
+        public Cliente Criar([FromBody] Cliente cliente)
+        {
+            return cliente.Salvar();
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public Cliente Atualizar(int id, [FromBody] Cliente cliente)
+        {
+            cliente.Id = id;
+            return cliente.Atualizar();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public void Excluir(int id)
+        {
+            Cliente.Excluir(id);
         }
     }
 }
